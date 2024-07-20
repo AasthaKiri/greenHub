f263b76fcc0af2b02faa9e784a54877eaf6fbe90
Archi Shah <62108268+ArchiShah4@users.noreply.github.com>
2024-07-20 12:10:43 -0400

    resolved errors

--- greenHub/greenHub/urls.py
+++ greenHub/greenHub/urls.py
@@ -1,10 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
-# urls.py
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('green/', include('green.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include('green.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
